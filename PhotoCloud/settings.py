1b366fdf3ad84f06e0fcfda5d6f2f60ab98901ac
slience_me <slience_me@foxmail.com>
2021-08-04 22:27:28 +0800

    修改了静态文件路径由nginx代理

--- PhotoCloud/settings.py
+++ PhotoCloud/settings.py
@@ -85,15 +85,15 @@ WSGI_APPLICATION = 'PhotoCloud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'tuyun',
-#         'USER': 'root',
-#         'PASSWORD': 'xian123',
-#         'PORT': '3306'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tuyun',
+        'USER': 'root',
+        'PASSWORD': 'xian123',
+        'PORT': '3306'
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -105,12 +105,12 @@ WSGI_APPLICATION = 'PhotoCloud.wsgi.application'
 #         'PORT': '3306',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
